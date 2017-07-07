2906f52c7968003b0cf62f0a1e95d3e1e255f18b
Victor de la Fouchardiere <victor.dlf@outlook.fr>
2017-07-07 17:21:11 +0200

    add portfolio

--- private/default/views/productions.php
+++ private/default/views/productions.php
@@ -1,82 +1,68 @@
 <?php include_once('/layouts/header.php'); ?>
 
-<div class="block__goals">
-	<h3>Mes Objectifs</h3>
-	<div class="row">
-		<div class="goal">
-			<div class="ico creation"></div>
-			<h5>Développement de A à Z</h5>
-			<p class="p__default">Créer des sites web complets coté utilisateur et coté serveur.
-			</p>
-		</div>
-		<div class="goal">
-			<div class="ico validation"></div>
-			<h5>Code propre et soigné</h5>
-			<p class="p__default">Programmer avec un code (HTML5 ; CSS3 ; PHP ; JS) propre et valide.
-			</p>
-		</div>
-		<div class="goal">
-			<div class="ico responsive"></div>
-			<h5>Responsive design adapté</h5>
-			<p class="p__default">Penser responsive quelque soit l'appareil (tablettes, smartphones, ordis...).
-			</p>
+<section class="container container_portfolio">
+	<div class="goals col col-hori-center nowrap">
+		<h3>Mes Objectifs</h3>
+		<div class="row row-verti-center row-hori-center nowrap">
+			<div class="block_goal col col-hori-center nowrap" id="development">
+				<span></span>
+				<h5>Développement de A à Z</h5>
+				<p>Créer des sites web complets coté utilisateur et coté serveur.</p>
+			</div>
+			<div class="block_goal col col-hori-center nowrap" id="cleaning">
+				<span></span>
+				<h5>Développement de A à Z</h5>
+				<p>Créer des sites web complets coté utilisateur et coté serveur.</p>
+			</div>
+			<div class="block_goal col col-hori-center nowrap" id="responsive">
+				<span></span>
+				<h5>Développement de A à Z</h5>
+				<p>Créer des sites web complets coté utilisateur et coté serveur.</p>
+			</div>
 		</div>
 	</div>
-</div>
-<section class="productions">
-	<h3>Portfolio</h3>
-	<div class="row">
-		<div class="prod">
-			<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-			<img src="./public/assets/imgComp/beparis.jpg" title="BeParis" alt="Accueil BeParis">
-			<div class="text">
-				<h6>BeParis</h6>
-				<p class="p__default">Dans le cadre d'un cours de Gestion de Projet à l'EEMI, BeParis a été développé en groupe de 12. Je me suis particulièrement occupé de la partie intégration.
-				</p>
-				<a href="http://www.beparis.hol.es/" target='_blank' title=""><i class="material-icons">link</i></a>
-			</div>
+	<div class="portfolio col col-hori-center nowrap">
+		<h3>Portfolio</h3>
+		<div class="row wrap">
+			<article id="beparis">
+				<div class="col col-hori-center nowrap">
+					<h6>BeParis</h6>
+					<p>Dans le cadre d'un cours de Gestion de Projet à l'EEMI, BeParis a été développé en groupe de 12. Je me suis particulièrement occupé de la partie intégration.</p>
+					<a href="http://www.beparis.hol.es/" target='_blank' title="">
+						<i class="material-icons">link</i>
+					</a>
+				</div>
+			</article>
+			<article id="tutobwim">
+				<div class="col col-hori-center nowrap">
+					<h6>TutoBwim.fr</h6>
+					<p>Ce site web développé par moi même via WordPress me permet de faire découvrir et d'élargir aux internautes leur connaissances en informatique avec des articles clairement rédigés.</p>
+					<a href="http://www.tutobwim.fr" target='_blank' title="">
+						<i class="material-icons">link</i>
+					</a>
+				</div>
+			</article>
+			<article id="codepen">
+				<div class="col col-hori-center nowrap">
+					<h6>TutoBwim.fr</h6>
+					<p>Ce site web développé par moi même via WordPress me permet de faire découvrir et d'élargir aux internautes leur connaissances en informatique avec des articles clairement rédigés.</p>
+					<a href="http://www.tutobwim.fr" target='_blank' title="">
+						<i class="material-icons">link</i>
+					</a>
+				</div>
+			</article>
+			<article id="ficheandtricks">
+				<div class="col col-hori-center nowrap">
+					<h6>Fiche & Tricks</h6>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+					<a href="http://www.tutobwim.fr" target='_blank' title="">
+						<i class="material-icons">link</i>
+					</a>
+				</div>
+			</article>
 		</div>
-		<div class="prod">
-			<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-			<img src="./public/assets/imgComp/tutobwim.jpg" title="TutoBwim.fr" alt="Accueil TutoBwim.fr">
-			<div class="text">
-				<h6>TutoBwim</h6>
-				<p class="p__default">Ce site web développé par moi même via WordPress me permet de faire découvrir et d'élargir aux internautes leur connaissances en informatique avec des articles clairement rédigés. 
-				</p>
-				<a href="http://www.tutobwim.fr" target='_blank' title=""><i class="material-icons">link</i></a>
-			</div>
-		</div>
-		<div class="prod">
-			<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-			<img src="./public/assets/imgComp/codepen.jpg" title="CodePen" alt="Profil CodePen">
-			<div class="text">
-				<h6>Productions CodePen</h6>
-				<p class="p__default">Mon profil CodePen comprend des créations que je réalise quand j'ai le temps. Cela me permet d'améliorer mon apprentissage en frontend.
-				</p>
-				<a href="http://codepen.io/Viclafouch/" target='_blank' title=""><i class="material-icons">link</i></a>
-			</div>
-		</div>
-		<div class="prod">
-			<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-			<img src="./public/assets/imgComp/ineco.jpg" title="INECO" alt="Accueil INECO">
-			<div class="text">
-				<h6>INECO</h6>
-				<p class="p__default" >Ce site a été réalisé dans le cadre d'un projet de fin de première année d'études. Il comprends une partie frontend et une partie backend dans lesquelles j'y ai grandement contribué. La base de données n'est malheureusement plus disponible.
-				</p>
-				<a href="http://manaranche1.etudiant-eemi.com/perso/1A/INECO/index.php" target='_blank' title=""><i class="material-icons">link</i></a>
-			</div>
-		</div>
-		<div class="prod">
-			<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-			<img src="./public/assets/imgComp/blog.jpg" title="Blog MVC" alt="Accueil Blog">
-			<div class="text">
-				<h6>Blog MVC</h6>
-				<p class="p__default">Petit blog dans lequel j'améliore mes compétences en PHP/MySQL et plus particulièrement dans une structure MVC.
-				</p>
-				<a href="http://fouchardiere.etudiant-eemi.com/perso/2APHPMYSQL/2ADEV-delafouchardiere-victor/" target='_blank' title=""><i class="material-icons">link</i></a>
-			</div>
-		</div>
-	</div>	
+	</div>
 </section>
 
 <?php include_once('/layouts/footer.php'); ?>
