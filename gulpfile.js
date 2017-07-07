5c4097bfb69aeb469782d78cda07c3696616dfe4
Victor de la Fouchardière <victor.dlf@outlook.fr>
2017-07-07 09:31:52 +0200

    add image / scss

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('styles', function() {
 		.pipe(compass({
 		 	css: 'public/assets/css',
 			sass: 'public/assets/scss',
-			image: 'public/assets/img'
+			image: 'public/assets/imgComp'
 		}))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     	.pipe(gulp.dest('public/assets/css'))
