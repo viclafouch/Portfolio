519f42679813abb449089c4da2601aa101d99820
Victor de la Fouchardiere <victor.dlf@outlook.fr>
2017-07-07 18:33:57 +0200

    change shadows

--- private/default/views/competences.php
+++ private/default/views/competences.php
@@ -1,145 +1,116 @@
 <?php include_once('/layouts/header.php'); ?>
 
-<div class="block__title competences">
-	<h3>Compétences</h3>
-</div>
-<section class="skills">
-	<h4>Langages utilisés</h4>
-	<div class="row">
-		<div class="skill-card">
-		  <div class="skill-card__header html">
-		  	<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-		  	<img class="skill-card__icon" src="./public/assets/imgComp/html.png" title="HTML5" alt="HTML5 Logo"/>
-		  </div>
-		  <section class="skill-card__body">
-		    <h2 class="skill-card__title">html</h2><span class="skill-card__duration">2 ans d'expérience</span>
-		    <ul class="skill-card__knowledge">
-		      <li>HTML5</li>
-		      <li>Sémantique</li>
-		      <li>SVG</li>
-		    </ul>
-		  </section>
+<section class="container container_competences">
+	<header>
+		<h3>Compétences</h3>
+	</header>
+	<div class="content_block">
+		<div class="skills languages">
+			<h4>Langages utilisés</h4>
+			<div class="row wrap row-verti-center row-hori-center">
+				<article class="skill_card" id="html">
+		 			<header>
+		  				<img src="./public/assets/imgComp/html.png" title="HTML5" alt="HTML5 Logo"/>
+		 			</header>
+		  			<footer>
+		    			<h5>html</h5>
+		    			<span>2 ans d'expérience</span>
+					    <ul>
+							<li>HTML5</li>
+							<li>Sémantique</li>
+							<li>SVG</li>
+					    </ul>
+					</footer>
+				</article>
+				<article class="skill_card" id="css">
+		 			<header>
+		  				<img src="./public/assets/imgComp/css.png" title="HTML5" alt="HTML5 Logo"/>
+		 			</header>
+		  			<footer>
+		    			<h5>css</h5>
+		    			<span>2 ans d'expérience</span>
+					    <ul>
+							<li>CSS3</li>
+		      				<li>Responsive</li>
+		      				<li>SASS</li>
+					    </ul>
+					</footer>
+				</article>
+				<article class="skill_card" id="js">
+		 			<header>
+		  				<img src="./public/assets/imgComp/js.png" title="HTML5" alt="HTML5 Logo"/>
+		 			</header>
+		  			<footer>
+		    			<h5>js</h5>
+		    			<span>2 ans d'expérience</span>
+					    <ul>
+							<li>Javascript</li>
+							<li>DOM</li>
+							<li>Ajax</li>
+					    </ul>
+					</footer>
+				</article>
+				<article class="skill_card" id="php">
+		 			<header>
+		  				<img src="./public/assets/imgComp/php.png" title="HTML5" alt="HTML5 Logo"/>
+		 			</header>
+		  			<footer>
+		    			<h5>php</h5>
+		    			<span>2 ans d'expérience</span>
+					    <ul>
+							<li>Objet</li>
+							<li>MVC</li>
+							<li>MYSQL</li>
+					    </ul>
+					</footer>
+				</article>
+			</div>
+			<h4>Frameworks utilisés</h4>
+			<div class="row wrap row-verti-center row-hori-center">
+				<article class="skill_card" id="compass">
+		 			<header>
+		  				<img src="./public/assets/imgComp/compass.png" title="HTML5" alt="HTML5 Logo"/>
+		 			</header>
+		  			<footer>
+		    			<h5>compass</h5>
+		    			<span>2 ans d'expérience</span>
+					    <ul>
+							<li>SCSS</li>
+							<li>Nesting</li>
+							<li>@Mixins</li>
+					    </ul>
+					</footer>
+				</article>
+				<article class="skill_card" id="jquery">
+		 			<header>
+		  				<img src="./public/assets/imgComp/jquery.png" title="HTML5" alt="HTML5 Logo"/>
+		 			</header>
+		  			<footer>
+		    			<h5>jquery</h5>
+		    			<span>2 ans d'expérience</span>
+					    <ul>
+							<li>SCSS</li>
+							<li>Nesting</li>
+							<li>@Mixins</li>
+					    </ul>
+					</footer>
+				</article>
+				<article class="skill_card" id="bootstrap">
+		 			<header>
+		  				<img src="./public/assets/imgComp/bootstrap.svg" title="HTML5" alt="HTML5 Logo"/>
+		 			</header>
+		  			<footer>
+		    			<h5>bootstrap</h5>
+		    			<span>2 ans d'expérience</span>
+					    <ul>
+							<li>SCSS</li>
+							<li>Nesting</li>
+							<li>@Mixins</li>
+					    </ul>
+					</footer>
+				</article>
+			</div>
 		</div>
-		<div class="skill-card">
-		  <div class="skill-card__header css">
-		  	<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-		  	<img class="skill-card__icon" src="./public/assets/imgComp/css.png" title="CSS3" alt="CSS3 Logo"/>
-		  </div>
-		  <section class="skill-card__body">
-		    <h2 class="skill-card__title">css</h2><span class="skill-card__duration">2 ans d'expérience</span>
-		    <ul class="skill-card__knowledge">
-		      <li>CSS3</li>
-		      <li>Responsive</li>
-		      <li>SASS</li>
-		    </ul>
-		  </section>
-		</div>
-		<div class="skill-card">
-
-		  <div class="skill-card__header js">
-		  	<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-		  	<img class="skill-card__icon" src="./public/assets/imgComp/js.png" title="JS" alt="Javacript Logo"/>
-		  </div>
-		  <section class="skill-card__body">
-		    <h2 class="skill-card__title">js</h2><span class="skill-card__duration">1 ans d'expérience</span>
-		    <ul class="skill-card__knowledge">
-		      <li>Javascript</li>
-		      <li>DOM</li>
-		      <li>Ajax</li>
-		    </ul>
-		  </section>
-		</div>
-		<div class="skill-card">
-		  <div class="skill-card__header php">
-		  	<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-		  	<img class="skill-card__icon" src="./public/assets/imgComp/php.svg" title="PHP5" alt="PHP Logo"/>
-		  </div>
-		  <section class="skill-card__body">
-		    <h2 class="skill-card__title">php</h2><span class="skill-card__duration">1 ans d'expérience</span>
-		    <ul class="skill-card__knowledge">
-		      <li>PHP5</li>
-		      <li>MVC</li>
-		      <li>MYSQL</li>
-		    </ul>
-		  </section>
-		</div>
-		<div class="skill-card">
-		  <div class="skill-card__header wordpress">
-		  	<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-		  	<img class="skill-card__icon" src="./public/assets/imgComp/wordpress.png" title="Wordpress" alt="Wordpress Logo"/>
-		  </div>
-		  <section class="skill-card__body">
-		    <h2 class="skill-card__title">wordpress</h2><span class="skill-card__duration">4 mois d'expérience</span>
-		    <ul class="skill-card__knowledge">
-		      <li>Wordpress</li>
-		      <li>Les bases</li>
-		      <li></li>
-		    </ul>
-		  </section>
-		</div>
-	</div>
-	<h4>Frameworks</h4>
-	<div class="row frameworks">
-		<div class="skill-card">
-		  <div class="skill-card__header compass">
-		  	<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-		  	<img class="skill-card__icon" src="./public/assets/imgComp/compass.png" title="Compass" alt="Compass Logo"/>
-		  </div>
-		  <section class="skill-card__body">
-		    <h2 class="skill-card__title">compass</h2><span class="skill-card__duration">6 mois d'expérience</span>
-		    <ul class="skill-card__knowledge">
-		      <li>SCSS</li>
-		      <li>Nesting</li>
-		      <li>@Mixins</li>
-		    </ul>
-		  </section>
-		</div>
-		<div class="skill-card">
-		  <div class="skill-card__header jquery">
-		  	<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-		  	<img class="skill-card__icon" src="./public/assets/imgComp/jquery.png" title="JQuery" alt="JQuery Logo"/>
-		  </div>
-		  <section class="skill-card__body">
-		    <h2 class="skill-card__title">jquery</h2><span class="skill-card__duration">1 ans d'expérience</span>
-		    <ul class="skill-card__knowledge">
-		      <li>jQuery v3.1</li>
-		      <li>Animations</li>
-		      <li>Ajax</li>
-		    </ul>
-		  </section>
-		</div>
-		<div class="skill-card">
-		  <div class="skill-card__header bootstrap">
-		  	<span class="tap"><img src="./public/assets/imgComp/doubletap.svg" alt="Tap"></span>
-		  	<img class="skill-card__icon" src="./public/assets/imgComp/bootstrap.svg" title="Bootstrap" alt="Bootstrap Logo"/>
-		  </div>
-		  <section class="skill-card__body">
-		    <h2 class="skill-card__title">bootstrap</h2><span class="skill-card__duration">2 mois d'expérience</span>
-		    <ul class="skill-card__knowledge">
-		      <li>Bootstrap v3.3</li>
-		      <li>Sémantique</li>
-		      <li>Les bases</li>
-		    </ul>
-		  </section>
-		</div>
-	</div>
-</section>
-<section class="mais_aussi">
-	<div class="left">
-		<h4>Langue</h4>
-		<img src="./public/assets/imgComp/english-flag.svg" alt="">
-		<p class="p__default">Anglais (Intermédiaire - B1)</p>
-	</div>
-	<div class="middle"></div>
-	<div class="right">
-		<h4>Centres d'interêt</h4>
-		<ul class="centre">
-			<li>Tennis de table</li>
-			<li>Batterie</li>
-			<li>Rock</li>
-			<li>Voyages</li>
-			<li>Nouvelles technologies</li>
-		</ul>
 	</div>
 </section>
 
