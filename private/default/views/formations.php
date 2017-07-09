0f10f0a6e7d7a8e319716a0fed434c4e04d0a776
Victor de la Fouchardière <victor.dlf@outlook.fr>
2017-07-09 12:21:22 +0200

    improvments

--- private/default/views/formations.php
+++ private/default/views/formations.php
@@ -6,49 +6,61 @@
 		<p>Ce que j'apprends</p>
 	</header>
 	<div class="content_block">
-		<div class="block col col-hori-center nowrap" id="eemi">
-			<img src="./public/assets/imgComp/eemi-logo.png" alt="">
-			<div class="description">
+		<div class="block row row-hori-center nowrap" id="eemi">
+			<div class="thumbnail">
+				<img src="./public/assets/imgComp/eemi-logo.png" alt="">
+			</div>
+			<div class="description col nowrap">
 				<p class="date">
 					<i class="fa fa-calendar" aria-hidden="true"></i>
 					<span>Depuis <u>Septembre 2015</u></span>
 				</p>
-				<h4>EEMI<span></span></h4>
+				<div class="row row-hori-between nowrap row-verti-center">
+					<h4>EEMI</h4>
+					<a class="button_default" title="" target="_blank" href="http://www.eemi.com/">A propos</a>
+				</div>
 				<ul>
 					<li>- 2ème année à l'Ecole Européenne des Métiers de L'Internet</li>
 					<li>- Connaissances acquises dans : Webmarketing / Communication digitale / Webdesign / Développement web / Gestion de Projet</li>
 					<li>- Objectif : Licence + Master (BAC +5)</li>
 				</ul>
-				<a class="button_default" title="" target="_blank" href="http://www.eemi.com/">A propos</a>
 			</div>
 		</div>
-		<div class="block row nowrap" id="digitalActive">
-			<div class="thumbnail"></div>				
-			<div class="description col col-verti-center">
+		<div class="block row row-hori-center nowrap" id="digitalActive">
+			<div class="thumbnail">
+				<img src="./public/assets/imgComp/digital-active.png" alt="">
+			</div>
+			<div class="description col nowrap">
 				<p class="date">
-					<i class="material-icons">access_time</i>
+					<i class="fa fa-calendar" aria-hidden="true"></i>
 					<span>Obtenu en <u>2016</u></span>
 				</p>
-				<h4>Certification Google<span></span></h4>
+				<div class="row row-hori-between nowrap row-verti-center">
+					<h4>Certification Google</h4>
+					<a class="button_default" title="" target="_blank" href="http://digitalactive.withgoogle.com/">A propos</a>
+				</div>
 				<ul>
 					<li>Digital Active est une formation au marketing digital, gratuite et certifiante, proposée par Google.</li>
 				</ul>
-				<a class="button_default" title="" target="_blank" href="http://digitalactive.withgoogle.com/">A propos</a>
 			</div>
 		</div>
-		<div class="block row nowrap" id="bachelor">
-			<div class="thumbnail"></div>				
-			<div class="description col col-verti-center">
+		<div class="block row row-hori-center nowrap" id="bachelor">
+			<div class="thumbnail">
+				<img src="./public/assets/imgComp/saint-paul.png" alt="">
+			</div>
+			<div class="description col nowrap">
 				<p class="date">
-					<i class="material-icons">access_time</i>
+					<i class="fa fa-calendar" aria-hidden="true"></i>
 					<span>Obtenu en <u>2015</u></span>
 				</p>
-				<h4>BAC Economique et Social<span></span></h4>
+				<div class="row row-hori-between nowrap row-verti-center">
+					<h4>BAC Economique et Social</h4>
+					<a class="button_default" title="" target="_blank" href="http://www.lycee-saint-paul-lille.fr/">A propos</a>
+				</div>
 				<ul>
 					<li>- Diplôme du Baccalauréat Economique et Social (ES)</li>
 					<li>- Lycée Saint Paul - Lille - Mention AB</li>
 				</ul>
-				<a class="button_default" title="" target="_blank" href="http://www.lycee-saint-paul-lille.fr/">A propos</a>
 			</div>
 		</div>
 	</div>
