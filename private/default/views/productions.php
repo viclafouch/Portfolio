c27964b10246cb2cece6a531e4c4cadcd8b87fa8
Victor de la Fouchardière <victor.dlf@outlook.fr>
2017-07-09 18:27:24 +0200

    add popup portfolio

--- private/default/views/productions.php
+++ private/default/views/productions.php
@@ -10,7 +10,7 @@
 			<div class="col nowrap">
 				<header class="header_work">
 					<div class="overlay">
-						<span><i class="fa fa-link" aria-hidden="true"></i></span>
+						<span data-popup-work><i class="fa fa-link" aria-hidden="true"></i></span>
 					</div>
 					<img src="./public/assets/imgComp/codepen.jpg" alt="" title="" />
 				</header>
@@ -24,12 +24,12 @@
 			<div class="col nowrap">
 				<header class="header_work">
 					<div class="overlay">
-						<span><i class="fa fa-link" aria-hidden="true"></i></span>
+						<span data-popup-work><i class="fa fa-link" aria-hidden="true"></i></span>
 					</div>
-					<img src="./public/assets/imgComp/codepen.jpg" alt="" title="" />
+					<img src="./public/assets/imgComp/beparis.jpg" alt="" title="" />
 				</header>
 				<footer class="footer_work">
-					<h4>Codepen</h4>
+					<h4>BeParis</h4>
 					<p>Fronted</p>
 				</footer>
 			</div>
@@ -38,12 +38,12 @@
 			<div class="col nowrap">
 				<header class="header_work">
 					<div class="overlay">
-						<span><i class="fa fa-link" aria-hidden="true"></i></span>
+						<span data-popup-work><i class="fa fa-link" aria-hidden="true"></i></span>
 					</div>
-					<img src="./public/assets/imgComp/codepen.jpg" alt="" title="" />
+					<img src="./public/assets/imgComp/tutobwim.jpg" alt="" title="" />
 				</header>
 				<footer class="footer_work">
-					<h4>Codepen</h4>
+					<h4>TutoBwim.fr</h4>
 					<p>Fronted</p>
 				</footer>
 			</div>
@@ -52,12 +52,12 @@
 			<div class="col nowrap">
 				<header class="header_work">
 					<div class="overlay">
-						<span><i class="fa fa-link" aria-hidden="true"></i></span>
+						<span data-popup-work><i class="fa fa-link" aria-hidden="true"></i></span>
 					</div>
-					<img src="./public/assets/imgComp/codepen.jpg" alt="" title="" />
+					<img src="./public/assets/imgComp/ineco.jpg" alt="" title="" />
 				</header>
 				<footer class="footer_work">
-					<h4>Codepen</h4>
+					<h4>Ineco</h4>
 					<p>Fronted</p>
 				</footer>
 			</div>
@@ -66,16 +66,31 @@
 			<div class="col nowrap">
 				<header class="header_work">
 					<div class="overlay">
-						<span><i class="fa fa-link" aria-hidden="true"></i></span>
+						<span data-popup-work><i class="fa fa-link" aria-hidden="true"></i></span>
 					</div>
-					<img src="./public/assets/imgComp/codepen.jpg" alt="" title="" />
+					<img src="./public/assets/imgComp/blog.jpg" alt="" title="" />
 				</header>
 				<footer class="footer_work">
-					<h4>Codepen</h4>
+					<h4>Blog MVC</h4>
 					<p>Fronted</p>
 				</footer>
 			</div>
 		</article>
+	</div>
+	<div class="popup">
+		<div class="popup_overlay"></div>
+		<div class="popup_container">
+			<div class="col nowrap">
+				<h3>TutoBwim.fr</h3>
+				<div class="screen">
+					<img src="./public/assets/imgComp/ficheandtricks.png" alt="">
+				</div>
+				<div class="content col nowrap col-hori-center">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad ut ullam error inventore dolore doloribus vitae ipsum esse itaque, id dolorem at laboriosam quasi totam optio distinctio dolorum, est vel.</p>
+					<a href="#" title="" class="button_default">Accéder au site</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
 
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,65 +5,14 @@
 // Your code will minify in /min
 
 // Barre de loader verticale
-$(function() {
-    $(window).scroll(
-        function() {
-            if ($(this).scrollTop() > 700) {
-                $('.trait-actif').css('height', '33.3%');
-                // $('#Google').animate({ opacity: 1}, 400);
-            } else {
-                $('.trait-actif').css('height', '0%');
-            }
-            if ($(this).scrollTop() > 1000) {
-                $('.trait-actif').css('height', '66.6%').fadeIn('slow');
-                // $('#Youtube').animate({ opacity: 1 }, 400);
-            }
-            if ($(this).scrollTop() > 1300) {
-                $('.trait-actif').css('height', '100%').fadeIn('slow');
-                // $('#Carrefour').animate({ opacity: 1 }, 400);
-            }
-        }
-    );
+
+$(document).on('click', '.popup_overlay', function(){
+    if (popup) {
+        $('.popup').removeClass('active');
+    }
 });
 
-
-$(function() {
-    $(window).scroll(
-        function() {
-            if ($(this).scrollTop() > 300) {
-                $('.creation').addClass('fadeIn');
-            }
-            if ($(this).scrollTop() > 300) {
-                $('.validation').addClass('fadeIn');
-            }
-            if ($(this).scrollTop() > 300) {
-                $('.responsive').addClass('fadeIn');
-            }
-        }
-    );
-});
-
-$(document).ready(function() {
-    var offset = 220;
-    var duration = 300;
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-            $('.haut').fadeIn(duration);
-        } else {
-            $('.haut').fadeOut(duration);
-        }
-    });
-    $('.haut').click(function(event) {
-        event.preventDefault();
-        $('html, body').animate({ scrollTop: 0 }, duration);
-        return false;
-    })
-});
-
-
-$(document).ready(function(){
-   $('.menu').children().click(function () {
-      $(".active").removeClass("active");
-      $(this).addClass("active");
-   });
+$(document).on('click', '[data-popup-work]', function(){
+    $('.popup').addClass('active');
+     popup = true;
 });
