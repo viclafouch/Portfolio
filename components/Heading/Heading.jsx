070c550a628bfe828cc551a16d88ac31c4fec763
Victor <victor@seald.io>
2018-12-25 17:24:52 +0100

    fix nav responsive

--- components/Heading/Heading.jsx
+++ components/Heading/Heading.jsx
@@ -6,15 +6,17 @@ const Heading = () => (
   <div className="subheading p-shadow">
     <div className="container flex-me flex-vertical flex-align flex-justify-center">
       <Link href="/">
-        <a className="sub-link">
+        <a className="subheading-link-logo center-text">
           <img
             src="/static/logo-de-la-fouchardiere-victor.png"
             alt="Victor de la Fouchardiere Logo"
           />
         </a>
       </Link>
-      <h1 className="sub-h1 sub-center">Besoin d'un développeur web&nbsp;?</h1>
-      <p className="sub-p sub-center">
+      <h1 className="subheading-title center-text">
+        Besoin d'un développeur web&nbsp;?
+      </h1>
+      <p className="subheading-subtitle center-text">
         Disponible pour vous aider dans le développement de votre projet, je
         vous invite à regarder ci-dessous l'ensemble de mes références pour
         mieux connaitre mon profil.
--- components/Layout/Nav/Nav.jsx
+++ components/Layout/Nav/Nav.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
 import './nav.scss'
+import { withRouter } from 'next/router'
+import { Children } from 'react'
 
 const links = [
   { text: 'Accueil', href: '/' },
@@ -10,46 +12,86 @@ const links = [
   { text: 'Contact', href: '/contact' }
 ]
 
+const ActiveLink = withRouter(({ router, children, ...props }) => {
+  const child = Children.only(children)
+
+  let className = child.props.className || ''
+  if (router.pathname === props.href && props.activeClassName) {
+    className = `${className} ${props.activeClassName}`.trim()
+  }
+
+  delete props.activeClassName
+
+  return <Link {...props}>{React.cloneElement(child, { className })}</Link>
+})
+
 export class Nav extends Component {
   constructor() {
     super()
     this.state = {
       menuOpened: false
     }
+    this.list = React.createRef()
+    this.burger = React.createRef()
     this.toggleNavigation = this.toggleNavigation.bind(this)
   }
 
   toggleNavigation() {
-    return this.setState({
-      menuOpened: !this.state.menuOpened
-    })
+    return this.setState(
+      {
+        menuOpened: !this.state.menuOpened
+      },
+      () => {
+        if (this.state.menuOpened) {
+          const listWidth = this.list.current.offsetWidth
+          const burgerWidth = this.burger.current.offsetWidth
+          const translationX = listWidth / 2 - (burgerWidth - 5) // padding-right + width burger / 2
+          this.burger.current.style.transform = `translateX(-${translationX}px)`
+        } else {
+          this.burger.current.style.transform = 'translateX(0px)'
+        }
+      }
+    )
   }
 
   render() {
     return (
-      <nav className="navigation">
-        <div className="container">
-          <div
-            className={
-              'nav-burger ' + this.state.menuOpened ? 'menu-opened' : ''
-            }
-            onClick={this.toggleNavigation}
-          >
-            <span />
+      <nav className="navigation p-shadow">
+        <div className="container flex-me flex-align">
+          <a href="/">
+            <img
+              src="/static/logo-de-la-fouchardiere-victor.png"
+              alt="Logo Victor de la Fouchardière"
+            />
+          </a>
+          <div>
+            <div
+              ref={this.burger}
+              className={
+                'nav-burger ' + (this.state.menuOpened ? 'menu-opened' : '')
+              }
+              onClick={this.toggleNavigation}
+            >
+              <span />
+            </div>
+            <ul
+              ref={this.list}
+              className={
+                'nav-links-list ' + (this.state.menuOpened ? 'active' : '')
+              }
+            >
+              {links.map((link, index) => (
+                <li key={index} className="nav-links-list-item">
+                  <ActiveLink href={link.href} activeClassName="active">
+                    <a className="nav-links-list-item-href">{link.text}</a>
+                  </ActiveLink>
+                </li>
+              ))}
+            </ul>
+            {this.state.menuOpened && (
+              <div className="nav-overlay" onClick={this.toggleNavigation} />
+            )}
           </div>
-          <ul
-            className={
-              'nav-links-list ' + this.state.menuOpened ? 'active' : ''
-            }
-          >
-            {links.map((link, index) => (
-              <li key={index} className="nav-links-list-item">
-                <Link href={link.href}>
-                  <a className="nav-links-list-item-href">{link.text}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
         </div>
       </nav>
     )
