32c856f7eca8e6c6c2fad5af047dbb3ec1ee187e
viclafouch <victor.dlf@outlook.fr>
2019-02-17 13:54:18 +0100

    meta

--- pages/_document.js
+++ pages/_document.js
@@ -24,11 +24,11 @@ export default class MyDocument extends Document {
           />
           {/* Facebook/Linkedin */}
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="Portfolio de ctor de la Fouchardière" />
+          <meta property="og:title" content="Portfolio de Victor de la Fouchardière" />
           <meta property="og:description" content="Besoin d'un développeur web ? Découvrez le profil de Victor de la Fouchardiere" />
           <meta property="og:url" content="https://www.victor-de-la-fouchardiere.fr" />
           <meta property="og:site_name" content="Victor de la Fouchardière | Portfolio" />
-          <meta property="og:image" content="https://www.victor-de-la-fouchardiere.fr/victor-de-la-fouchardiere.png" />
+          <meta property="og:image" content="https://www.victor-de-la-fouchardiere.fr/static/victor-de-la-fouchardiere.png" />
           <meta property="fb:app_id" content="124333614845929" />
           <meta name="robots" content="noodp" />
           <link rel="icon" type="image/png" href="/static/favicon.png" />
