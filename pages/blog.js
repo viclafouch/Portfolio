acabe3415aba19bc5aa87133e530a6305251ce88
viclafouch <victor.dlf@outlook.fr>
2020-06-05 12:42:39 +0200

    fix: open article blog _blank

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,9 @@
+import moment from 'moment'
 import Page from 'components/Page/Page'
 import Title from 'components/Title/Title'
 import Meta from 'components/Meta/Meta'
+
+moment.locale('fr')
 
 function Blog({ posts }) {
   return (
@@ -14,11 +17,11 @@ function Blog({ posts }) {
         <Title title="Blog" subtitle="Ce que j'écris" />
         <div className="blog-list">
           {posts.map((post, index) => (
-            <article key={post.id} className="blog-list-item">
+            <article key={post.id} className="blog-list-item" data-id={post.id}>
               <div role="presentation">
                 <div className="blog-article-top">
                   {index === 0 && (
-                    <a href={post.url}>
+                    <a href={post.url} target="_blank" rel="noreferrer">
                       <div
                         className="blog-article-cover"
                         style={{
@@ -32,13 +35,15 @@ function Blog({ posts }) {
                       <img className="blog-author-pic" src={post.user.profile_image_90} alt={post.user.name} />
                       <div className="meta-texts">
                         <span className="meta-author-name">{post.user.name}</span>
-                        <a className="meta-author-date" href={post.url}>
-                          <time>16 June 2020</time>
+                        <a className="meta-author-date" href={post.url} target="_blank" rel="noreferrer">
+                          <time>
+                            {moment(post.published_timestamp).format('D MMM YYYY')} ({moment(post.published_timestamp).fromNow()})
+                          </time>
                         </a>
                       </div>
                     </div>
                     <div className="blog-meta-right">
-                      <a href={post.url}>
+                      <a href={post.url} target="_blank" rel="noreferrer">
                         <img src="/images/devto.svg" alt="Dev.to" />
                       </a>
                     </div>
@@ -46,7 +51,9 @@ function Blog({ posts }) {
                 </div>
                 <div className="blog-article-body">
                   <h2 className="body-article-title">
-                    <a href={post.url}>{post.title}</a>
+                    <a href={post.url} target="_blank" rel="noreferrer">
+                      {post.title}
+                    </a>
                   </h2>
                   <ul className="blog-list-tags">
                     {post.tag_list.map((tag, index) => (
