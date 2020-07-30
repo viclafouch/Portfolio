95a0bd5d4a8616c7a50e3ba67d288a66e6f3f391
viclafouch <victor.dlf@outlook.fr>
2020-07-30 22:47:24 +0200

    fix: favicon 404

--- pages/_document.js
+++ pages/_document.js
@@ -28,11 +28,11 @@ export default class MyDocument extends Document {
           <meta property="og:site_name" content="Victor de la Fouchardière | Portfolio" />
           <meta property="og:image" content="https://www.victor-de-la-fouchardiere.fr/victordelafouchardiere.png" />
           <meta property="fb:app_id" content="124333614845929" />
-          <link rel="icon" type="image/png" href="favicon.png" />
-          <link rel="shortcut icon" type="image/x-icon" href="favicon-16x16.png" />
-          <link rel="apple-touch-icon" href="icon-iphone.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="icon-ipad.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="icon-retina.png" />
+          <link rel="icon" type="image/png" href="/favicon.png" />
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon-16x16.png" />
+          <link rel="apple-touch-icon" href="/icon-iphone.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/icon-ipad.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/icon-retina.png" />
           <meta name="google-site-verification" content="7_XZ1bK94NNQLjHFdKXCa9FjsNaOZGx76uQgQzDPIIw" />
           <link rel="stylesheet" href="/reset.css" />
 
