d0070cd5622324f66b98c387e4a754f893cd325a
viclafouch <victor.dlf@outlook.fr>
2023-10-16 11:20:20 +0200

    chore: udpate deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,10 @@
-const {
-  sortImports
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-} = require('@viclafouch/eslint-config-viclafouch/rules/sort-imports')
-
 /**
  * @type {import("eslint").Linter.Config}
  */
 module.exports = {
   extends: [
-    "@viclafouch/eslint-config-viclafouch",
-    "@viclafouch/eslint-config-viclafouch/next",
-    "@viclafouch/eslint-config-viclafouch/prettier",
-  ],
-  overrides: [sortImports(__dirname)]
+    '@viclafouch/eslint-config-viclafouch',
+    '@viclafouch/eslint-config-viclafouch/next',
+    '@viclafouch/eslint-config-viclafouch/prettier'
+  ]
 }
